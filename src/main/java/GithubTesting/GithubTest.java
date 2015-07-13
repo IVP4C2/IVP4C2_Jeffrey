20d43869488787007c7f2d3d4b16003bb09fbf87
Jeffrey Oomen <jeffrey-140@hotmail.com>
2015-07-13 15:32:37 +0200

    eerste versie

---
+++ src/main/java/GithubTesting/GithubTest.java
@@ -0,0 +1,20 @@
+package GithubTesting;
+
+public class GithubTest {
+	
+	String naam; 
+	int leeftijd;
+	
+	public String getNaam() {
+		return naam;
+	}
+	public void setNaam(String naam) {
+		this.naam = naam;
+	}
+	public int getLeeftijd() {
+		return leeftijd;
+	}
+	public void setLeeftijd(int leeftijd) {
+		this.leeftijd = leeftijd;
+	}
+}
